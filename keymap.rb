5c4e40edd2de40289ab3664b1b156b8869976c01
HASUMI Hitoshi <hasumikin@gmail.com>
2021-06-01 15:52:32 +0900

    Conforms to Keyboard#add_layer

--- keymap.rb
+++ keymap.rb
@@ -20,39 +20,37 @@ kbd.init_pins(
 )
 
 # default layer should be added at first
-kbd.add_layer :default, [
-  %i(KC_F1     KC_F2   KC_F3   KC_F4       KC_F5     KC_F6    KC_NO  KC_NO   KC_F7     KC_F8     KC_F9     KC_F10   KC_F11    KC_F12),
-  %i(KC_ESCAPE KC_Q    KC_W    KC_E        KC_R      KC_T     KC_NO  KC_NO   KC_Y      KC_U      KC_I      KC_O     KC_P      KC_MINUS),
-  %i(KC_TAB    KC_A    KC_S    KC_D        KC_F      KC_G     KC_NO  KC_NO   KC_H      KC_J      KC_K      KC_L     KC_SCOLON KC_BSPACE),
-  %i(KC_LSFT   KC_Z    KC_X    KC_C        KC_V      KC_B     KC_NO  KC_NO   KC_N      KC_M      KC_COMMA  KC_DOT   KC_SLASH  KC_RSFT),
-  %i(KC_NO     KC_NO   KC_NO   ALT_AT      CTL_EQ  LOWER_SPC  KC_AT  KC_UNDS RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO),
+kbd.add_layer :default, %i[
+  KC_F1     KC_F2   KC_F3   KC_F4       KC_F5     KC_F6    KC_NO  KC_NO   KC_F7     KC_F8     KC_F9     KC_F10   KC_F11    KC_F12
+  KC_ESCAPE KC_Q    KC_W    KC_E        KC_R      KC_T     KC_NO  KC_NO   KC_Y      KC_U      KC_I      KC_O     KC_P      KC_MINUS
+  KC_TAB    KC_A    KC_S    KC_D        KC_F      KC_G     KC_NO  KC_NO   KC_H      KC_J      KC_K      KC_L     KC_SCOLON KC_BSPACE
+  KC_LSFT   KC_Z    KC_X    KC_C        KC_V      KC_B     KC_NO  KC_NO   KC_N      KC_M      KC_COMMA  KC_DOT   KC_SLASH  KC_RSFT
+  KC_NO     KC_NO   KC_NO   ALT_AT      CTL_EQ  LOWER_SPC  KC_AT  KC_UNDS RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO
 ]
-kbd.add_layer :raise, [
-  %i(KC_F1     KC_F2   KC_F3   KC_F4       KC_F5     KC_F6    KC_NO  KC_NO   KC_F7     KC_F8     KC_F9     KC_F10   KC_F11    KC_F12),
-  %i(KC_GRAVE  KC_EXLM KC_AT   KC_HASH     KC_DLR    KC_PERC  KC_NO  KC_NO   KC_CIRC   KC_AMPR   KC_ASTER  KC_LPRN  KC_RPRN   KC_MIUNS),
-  %i(KC_TAB    KC_LABK KC_LCBR KC_LBRACKET KC_LPRN   KC_QUOTE KC_NO  KC_NO   KC_LEFT   KC_DOWN   KC_UP     KC_RIGHT KC_UNDS   KC_PIPE),
-  %i(KC_LSFT   KC_RABK KC_RCBR KC_RBRACKET KC_RPRN   KC_DQUO  KC_NO  KC_NO   KC_TILD   KC_BSLASH KC_COMMA  KC_DOT   KC_SLASH  KC_RSFT),
-  %i(KC_NO     KC_NO   KC_NO   ALT_AT      CTL_EQ  LOWER_SPC  KC_AT  KC_UNDS RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO),
+kbd.add_layer :raise, %i[
+  KC_F1     KC_F2   KC_F3   KC_F4       KC_F5     KC_F6    KC_NO  KC_NO   KC_F7     KC_F8     KC_F9     KC_F10   KC_F11    KC_F12
+  KC_GRAVE  KC_EXLM KC_AT   KC_HASH     KC_DLR    KC_PERC  KC_NO  KC_NO   KC_CIRC   KC_AMPR   KC_ASTER  KC_LPRN  KC_RPRN   KC_MIUNS
+  KC_TAB    KC_LABK KC_LCBR KC_LBRACKET KC_LPRN   KC_QUOTE KC_NO  KC_NO   KC_LEFT   KC_DOWN   KC_UP     KC_RIGHT KC_UNDS   KC_PIPE
+  KC_LSFT   KC_RABK KC_RCBR KC_RBRACKET KC_RPRN   KC_DQUO  KC_NO  KC_NO   KC_TILD   KC_BSLASH KC_COMMA  KC_DOT   KC_SLASH  KC_RSFT
+  KC_NO     KC_NO   KC_NO   ALT_AT      CTL_EQ  LOWER_SPC  KC_AT  KC_UNDS RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO
 ]
-kbd.add_layer :lower, [
-  %i(KC_F1     KC_F2   KC_F3   KC_F4       KC_F5     KC_F6    KC_NO  KC_NO   KC_F7     KC_F8     KC_F9     KC_F10   KC_F11    KC_F12),
-  %i(KC_ESCAPE KC_1    KC_2    KC_3        KC_4      KC_5     KC_NO  KC_NO   KC_6      KC_7      KC_8      KC_9     KC_0      KC_MIUNS),
-  %i(KC_TAB    KC_F2   KC_F10  KC_F12      KC_LPRN   KC_QUOTE KC_NO  KC_NO   KC_DOT    KC_4      KC_5      KC_6     KC_PLUS   KC_BSPACE),
-  %i(KC_LSFT   KC_RABK KC_RCBR KC_RBRACKET KC_RPRN   KC_DQUO  KC_NO  KC_NO   KC_0      KC_1      KC_2      KC_3     KC_SLASH  KC_COMMA),
-  %i(KC_NO     KC_NO   KC_NO   ALT_AT      CTL_EQ  LOWER_SPC  KC_AT  KC_UNDS RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO),
+kbd.add_layer :lower, %i[
+  KC_F1     KC_F2   $C_F3   KC_F4       KC_F5     KC_F6    KC_NO  KC_NO   KC_F7     KC_F8     KC_F9     KC_F10   KC_F11    KC_F12
+  KC_ESCAPE KC_1    KC_2    KC_3        KC_4      KC_5     KC_NO  KC_NO   KC_6      KC_7      KC_8      KC_9     KC_0      KC_MIUNS
+  KC_TAB    KC_F2   KC_F10  KC_F12      KC_LPRN   KC_QUOTE KC_NO  KC_NO   KC_DOT    KC_4      KC_5      KC_6     KC_PLUS   KC_BSPACE
+  KC_LSFT   KC_RABK KC_RCBR KC_RBRACKET KC_RPRN   KC_DQUO  KC_NO  KC_NO   KC_0      KC_1      KC_2      KC_3     KC_SLASH  KC_COMMA
+  KC_NO     KC_NO   KC_NO   ALT_AT      CTL_EQ  LOWER_SPC  KC_AT  KC_UNDS RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO
 ]
 #
-#                   Your custom     Keycode or             Keycode (only modifiers)             Release time      Re-push time
-#                   key name        Array of Keycode       or Proc                              threshold(ms)     threshold(ms)
-#                                   or Proc                while you                            to consider as    to consider as
-#                                   when you click         keep press                           `click the key`   `hold the key`
-kbd.define_mode_key :ALT_AT,      [ :KC_AT,                :KC_LALT                           , 150,              150 ]
-kbd.define_mode_key :CTL_EQ,      [ :KC_EQUAL,             :KC_LCTL                           , 150,              150 ]
-kbd.define_mode_key :SPC_CTL,     [ %i(KC_SPACE KC_RCTL),  :KC_RCTL                           , 150,              150 ]
-kbd.define_mode_key :RAISE_ENT,   [ :KC_ENTER,             Proc.new { kbd.hold_layer :raise  }, 150,              150 ]
-kbd.define_mode_key :LOWER_SPC,   [ :KC_SPACE,             Proc.new { kbd.hold_layer :lower  }, 150,              150 ]
-#kbd.define_mode_key :ADJUST,      [ nil,                   Proc.new { kbd.hold_layer :adjust }, nil,              nil ]
-                                                            # ^^^^^^^^^^ `hold_layer` will "hold" layer while pressed
+#                   Your custom     Keycode or             Keycode (only modifiers)      Release time      Re-push time
+#                   key name        Array of Keycode       or Layer Symbol to be held    threshold(ms)     threshold(ms)
+#                                   or Proc                or Proc which will run        to consider as    to consider as
+#                                   when you click         while you keep press          `click the key`   `hold the key`
+kbd.define_mode_key :ALT_AT,      [ :KC_AT,                :KC_LALT,                     150,              150 ]
+kbd.define_mode_key :CTL_EQ,      [ :KC_EQUAL,             :KC_LCTL,                     150,              150 ]
+kbd.define_mode_key :SPC_CTL,     [ %i(KC_SPACE KC_RCTL),  :KC_RCTL,                     150,              150 ]
+kbd.define_mode_key :RAISE_ENT,   [ :KC_ENTER,             :raise,                       150,              150 ]
+kbd.define_mode_key :LOWER_SPC,   [ :KC_SPACE,             :lower,                       150,              150 ]
 
 # `before_report` will work just right before reporting what keys are pushed to USB host.
 # You can use it to hack data by adding an instance method to Keyboard class by yourself.
