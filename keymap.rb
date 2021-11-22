1fd629ae2c8c7c7850fa97cae33bdc2b807800ea
HASUMI Hitoshi <hasumikin@gmail.com>
2021-11-22 14:13:14 +0900

    bump up tp 0.9.6

--- keymap.rb
+++ keymap.rb
@@ -4,6 +4,14 @@ kbd = Keyboard.new
 # `split=` should happen before `init_pins`
 kbd.split = true
 
+# Caution!!!!
+#   - Keyboard#mutual_uart_at_my_own_risk is experimental.
+#     It may make the keyboard unstable and may break your chip
+#   - Make sure to disconnect the TRS(TRRS) cable when you change
+#     this configuration. Reconnect them after changing BOTH halves
+#   - Keys RGB_xxx are working on partner half only using this option
+# kbd.mutual_uart_at_my_own_risk = true
+
 # If your right hand the "anchor"
 # kbd.set_anchor(:right)
 
@@ -16,25 +24,25 @@ kbd.init_pins(
 
 # default layer should be added at first
 kbd.add_layer :default, %i[
-  KC_F1     KC_F2   KC_F3   KC_F4       KC_F5     KC_F6    KC_NO  KC_NO   KC_F7     KC_F8     KC_F9     KC_F10   KC_F11    KC_F12
+  RGB_MOD   KC_F2   KC_F3   KC_F4       KC_F5     KC_F6    KC_NO  KC_NO   KC_F7     KC_F8     KC_F9     KC_F10   KC_F11    KC_F12
   KC_ESCAPE KC_Q    KC_W    KC_E        KC_R      KC_T     KC_NO  KC_NO   KC_Y      KC_U      KC_I      KC_O     KC_P      KC_MINUS
   KC_TAB    KC_A    KC_S    KC_D        KC_F      KC_G     KC_NO  KC_NO   KC_H      KC_J      KC_K      KC_L     KC_SCOLON KC_BSPACE
   KC_LSFT   KC_Z    KC_X    KC_C        KC_V      KC_B     KC_NO  KC_NO   KC_N      KC_M      KC_COMMA  KC_DOT   KC_SLASH  KC_RSFT
-  KC_NO     KC_NO   KC_NO   ALT_AT      CTL_EQ  LOWER_SPC  KC_AT  KC_UNDS RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO
+  RGB_SPD   RGB_HUD RGB_SAD RGB_VAD     ALT_AT    CTL_EQ LOWER_SPC RAISE_ENT SPC_CTL KC_RGUI  RGB_VAI  RGB_SAI   RGB_HUI   RGB_SPI
 ]
 kbd.add_layer :raise, %i[
   KC_F1     KC_F2   KC_F3   KC_F4       KC_F5     KC_F6    KC_NO  KC_NO   KC_F7     KC_F8     KC_F9     KC_F10   KC_F11    KC_F12
   KC_GRAVE  KC_EXLM KC_AT   KC_HASH     KC_DLR    KC_PERC  KC_NO  KC_NO   KC_CIRC   KC_AMPR   KC_ASTER  KC_LPRN  KC_RPRN   KC_MIUNS
   KC_TAB    KC_LABK KC_LCBR KC_LBRACKET KC_LPRN   KC_QUOTE KC_NO  KC_NO   KC_LEFT   KC_DOWN   KC_UP     KC_RIGHT KC_UNDS   KC_PIPE
   KC_LSFT   KC_RABK KC_RCBR KC_RBRACKET KC_RPRN   KC_DQUO  KC_NO  KC_NO   KC_TILD   KC_BSLASH KC_COMMA  KC_DOT   KC_SLASH  KC_RSFT
-  KC_NO     KC_NO   KC_NO   ALT_AT      CTL_EQ  LOWER_SPC  KC_AT  KC_UNDS RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO
+  RGB_RMOD  RGB_HUD RGB_SAD RGB_VAD     ALT_AT    CTL_EQ LOWER_SPC RAISE_ENT SPC_CTL KC_RGUI  RGB_VAI  RGB_SAI   RGB_HUI   RGB_MOD
 ]
 kbd.add_layer :lower, %i[
   KC_F1     KC_F2   $C_F3   KC_F4       KC_F5     KC_F6    KC_NO  KC_NO   KC_F7     KC_F8     KC_F9     KC_F10   KC_F11    KC_F12
   KC_ESCAPE KC_1    KC_2    KC_3        KC_4      KC_5     KC_NO  KC_NO   KC_6      KC_7      KC_8      KC_9     KC_0      KC_MIUNS
   KC_TAB    KC_F2   KC_F10  KC_F12      KC_LPRN   KC_QUOTE KC_NO  KC_NO   KC_DOT    KC_4      KC_5      KC_6     KC_PLUS   KC_BSPACE
   KC_LSFT   KC_RABK KC_RCBR KC_RBRACKET KC_RPRN   KC_DQUO  KC_NO  KC_NO   KC_0      KC_1      KC_2      KC_3     KC_SLASH  KC_COMMA
-  KC_NO     KC_NO   KC_NO   ALT_AT      CTL_EQ  LOWER_SPC  KC_AT  KC_UNDS RAISE_ENT SPC_CTL   KC_RGUI  KC_NO     KC_NO     KC_NO
+  RGB_TOG   RGB_HUD RGB_SAD RGB_VAD     ALT_AT    CTL_EQ LOWER_SPC RAISE_ENT SPC_CTL KC_RGUI  RGB_VAI  RGB_SAI   RGB_HUI   RGB_TOG
 ]
 #
 #                   Your custom     Keycode or             Keycode (only modifiers)      Release time      Re-push time
@@ -62,9 +70,10 @@ rgb = RGB.new(
   32,   # size of backlight pixel
   false # 32bit data will be sent to a pixel if true while 24bit if false
 )
+sleep 1
 # Set an effect
 #  `nil` or `:off` for turning off
-rgb.effect = :rainbow_mood
+rgb.effect = :swirl
 # Set an action when you input
 #  `nil` or `:off` for turning off
 #rgb.action = :thunder
